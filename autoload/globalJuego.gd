d4bd2a0c88722426895f7c7422626183b920783b
Violeta Mortarini Forchetti <violemf@gmail.com>
2026-09-02 11:37:26 -0300

    BasePersonaje + globales

---
+++ autoload/globalJuego.gd
@@ -0,0 +1,10 @@
+extends Node
+
+#inventario global
+var inventario_jugador = [null, null, null, null, null, null] # inventario de maximo 6 slots
+
+#Seleccion de personaje
+var polloBasico = "res://scenes/pollos/pollo_modelo_1.tscn"
+
+
+#
