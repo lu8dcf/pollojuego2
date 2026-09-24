785cfa9becb494650c539ca7ea98692c07c10b0d
Heber Alonso <84638715+lu8dcf@users.noreply.github.com>
2026-09-24 20:14:00 -0300

    cambio de bala

--- scenes/enemy/enemy_0.gd
+++ scenes/enemy/enemy_0.gd
@@ -394,5 +394,5 @@ func _on_bigote_body_exited(_body: Node3D) -> void:
 func _on_danio_body_entered(_body: Node3D) -> void:
 	if not multiplayer.is_server(): # solo el servidor puede mover los enemigos
 		return
-	
+	print("pego")
 	queue_free()
